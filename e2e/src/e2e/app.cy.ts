e2104fb4018e8880a2e3914c47b2c1ff8587515c
William <williamw15@protonmail.com>
2023-02-09 10:01:06 -0500

    Initial commit

---
+++ e2e/src/e2e/app.cy.ts
@@ -0,0 +1,13 @@
+import { getGreeting } from '../support/app.po';
+
+describe('js4-final-project-app', () => {
+  beforeEach(() => cy.visit('/'));
+
+  it('should display welcome message', () => {
+    // Custom command example, see `../support/commands.ts` file
+    cy.login('my-email@something.com', 'myPassword');
+
+    // Function helper example, see `../support/app.po.ts` file
+    getGreeting().contains('Welcome js4-final-project-app');
+  });
+});
